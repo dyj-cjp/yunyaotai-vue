897eb07926f15b4105abc689b27c8ad238f81439
cjp <2582074719@qq.com>
2021-03-27 00:06:16 +0800

    main分支代码提交

--- src/views/profile/Profile.vue
+++ src/views/profile/Profile.vue
@@ -53,6 +53,12 @@
         <span class='message_text'>main default分支</span>
         <img class='purchase_right' src='../../assets/mine/right.png'>
       </div>
+                        <div class='message' @click="toMyMessage">
+        <img class='message_img' src='../../assets/mine/coln3.png'>
+        <span class='message_text'>main分支提交 </span>
+        <img class='purchase_right' src='../../assets/mine/right.png'>
+      </div>
+      
     </div>
     <!-- action="/api/file/upload" -->
     <!-- <el-upload
